4328885f217d4546cafe2a904880ce28487e587d
RadAdm <radadm@nowcast1.fmi.fi>
2022-02-04 11:07:44 +0200

    Added Dockerfile

---
+++ run_container_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export HDF5_USE_FILE_LOCKING=FALSE
+DOMAIN=${DOMAIN:-'ravake'}
+
+#List latest file in domain folder
+INPATH=/tutka/data/input_composites/$DOMAIN/
+LATEST_TIMESTAMP=`ls -t $INPATH | head -n1 | awk -F "_" '{print $1}'`
+
+TIMESTAMP=${TIMESTAMP:-${LATEST_TIMESTAMP}}
+
+#Output path
+#Read short hostname from server to use as output folder names.
+HOSTNAME=`hostname -s`
+NODE=${NODE:-${HOSTNAME}}
+OUTPATH=/tutka/data/dev/cache/radar/fmippn/${NODE}
+
+#Log path
+LOGPATH=/tutka/data/dev/cache/log/fmippn/${NODE}
+
+echo INPATH: $INPATH
+echo OUTPATH: $OUTPATH
+echo LOGPATH: $LOGPATH
+echo "Calculating PPN nowcast (testing callback function) for domain "$DOMAIN", timestamp "$TIMESTAMP
+
+#Mkdirs if log and outpaths have been cleaned
+mkdir -p $OUTPATH
+mkdir -p $LOGPATH
+
+# Build from Dockerfile
+#docker build -t fmippn .
+
+# --log-level=debug \
+#--user $(id -u):$(id -g) \
+
+# Run with volume mounts
+docker run \
+       --rm \
+       --env "timestamp=$TIMESTAMP" \
+       --env "domain=$DOMAIN" \
+       --security-opt label=disable \
+       -v  ${INPATH}:/input \
+       -v  ${OUTPATH}:/output \
+       -v  ${LOGPATH}:/log \
+       -v "$(pwd)"/fmippn/config/${DOMAIN}.json:/fmippn/config/${DOMAIN}.json \
+       fmippn:latest
+
